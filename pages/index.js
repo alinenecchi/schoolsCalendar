91c7335e07d4081ede688524724d653c388c604b
aline-necchi <alinenecchi@gmail.com>
2022-03-30 22:50:39 -0300

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import * as data from '../data/home';
-import SectionHero from '../components/organisms/section-hero';
 import Navbar from '../components/molecules/navbar';
+import Title from '../components/atoms/title';
 import css from '../utils/styles/home.module.scss';
-
-
 
 export default function Home(props) {
 
@@ -20,12 +17,12 @@ export default function Home(props) {
     {...other}
   >
 
-    <div class="stars" ></div>
-    <div class="stars2" ></div>
-    <div class="stars3" ></div>
+    <div className="stars" ></div>
+    <div className="stars2" ></div>
+    <div className="stars3" ></div>
     
-    <Navbar 
-        className={css['navbar']} 
-      />
+    <div className={css["title"]}>
+      <Title> Hello SchollsCalendar</Title>
+    </div>
   </div>;
 }
