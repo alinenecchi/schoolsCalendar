0138c758e2601d8e992139362f892c0ee623c034
aline-necchi <alinenecchi@gmail.com>
2022-04-22 16:32:09 -0300

    Adiciona hero banner

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import App from 'next/app';
 import Layout from '../utils/Layout';
-import { throttle, debounce } from 'utils/functions';
 import { PageProvider, PageContext } from 'providers/page';
 import packageJson from '../package.json';
 import 'utils/styles/_reset.scss';
