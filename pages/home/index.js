d5d0358b25f102c0e8260a1539c6c3b0d04c44d0
aline-necchi <alinenecchi@gmail.com>
2022-05-08 02:25:04 -0300

    REsolve error vercel

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,6 +1,4 @@
 import React from "react";
-import * as pageData from "data/home";
-import SectionCard from "../../components/organisms/section-card";
 import css from "./home.module.scss";
 
 export default function Home(props) {
@@ -8,7 +6,7 @@ export default function Home(props) {
 
   return (
     <div className={`${css["page__home-container"]} ${className}`} {...other}>
-      <SectionCard dataCard={pageData.dataCard} />
+      home
     </div>
   );
 }
