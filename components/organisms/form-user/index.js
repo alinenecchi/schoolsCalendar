75c989e00c40c7c292fe14ee307bf451d8b1a1b4
aline-necchi <alinenecchi@gmail.com>
2022-05-09 16:42:18 -0300

    Adiciona página de login e formulário de login

--- components/organisms/form-user/index.js
+++ components/organisms/form-user/index.js
@@ -1,145 +1,180 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import Input from '../../atoms/input';
-import ButtonHorizontal from '../../molecules/button-horizontal';
-import FormRadioGroup from '../../atoms/form-radio-group';
-import css from './FormUser.module.scss';
-import { isEmpty, omit } from 'lodash';
+import React, { useState, useCallback, useEffect } from "react";
+import Input from "../../atoms/input";
+import ButtonHorizontal from "../../molecules/button-horizontal";
+import FormRadioGroup from "../../atoms/form-radio-group";
+import CheckControl from "../../atoms/check-control";
+import css from "./FormUser.module.scss";
+import { isEmpty, omit } from "lodash";
+import { Card } from "@material-ui/core";
 
 function FormUser(props) {
   const {
     className = "",
-    civilStatus = [],
+    userStatus = [],
+    professor = "professor",
+    aluno = "aluno",
+    selected,
     children,
     onMoveNext,
     ...other
   } = props;
 
+  const options = [
+    { id: 1, name: "professor" },
+    { id: 2, name: "aluno" },
+  ];
 
   const [isFormIncomplete, setIsFormIncomplete] = useState(true);
-  const [state, setState]                       = useState("default");
-  const [lastError, setLastError]               = useState(null);
+  const [state, setState] = useState("default");
+  const [checkedList, setCheckedList] = React.useState(uncheckAll(options));
+  console.log(checkedList);
 
-  const [dataCard, setDataCard] = useState({
-    cvc: "",
-    expiry: "",
-    name: "",
-    number: "",
-  });
-
-  function onClose() {
-    setState("default");
+  function uncheckAll(options) {
+    return options.map((option) => ({
+      ...option,
+      checked: false,
+    }));
   }
 
+  function toggleOption(id, checked) {
+    return options.map((option) =>
+      option.id === id ? { ...option, checked } : option
+    );
+  }
+
+  const changeList = (id, checked) => {
+    const newCheckedList = toggleOption(id, checked);
+    setCheckedList(newCheckedList);
+  };
+
+  const [dataCard, setDataCard] = useState({
+    name: "",
+    password: "",
+    status: "",
+  });
+
+  console.log(dataCard);
+
   useEffect(() => {
-    const isUserEmpty = Object.values(dataCard).some(x => isEmpty(x));
-    if(isUserEmpty) {
+    const isUserEmpty = Object.values(dataCard).some((x) => isEmpty(x));
+    if (isUserEmpty) {
       setIsFormIncomplete(true);
     } else {
       setIsFormIncomplete(false);
     }
-  },
-  [dataCard]);
-
-  const handleChange = useCallback((event) => {
-    setDataCard({
-      ...dataCard,
-      [event.target.name]: event.target.value,
-    });
   }, [dataCard]);
+
+  const handleChange = useCallback(
+    (event) => {
+      setDataCard({
+        ...dataCard,
+        [event.target.name]: event.target.value,
+      });
+    },
+    [dataCard]
+  );
 
   const handleSubmit = (event) => {
     setState("loading");
     event.preventDefault();
 
-    const dataCard = {
+    const data = {
       name: dataCard.name,
       password: dataCard.password,
+      status: dataCard.status,
     };
 
-    const data = {
-      ...dataCardCredit
-    };
+    console.log(data);
 
     const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
     };
 
     fetch("/api/login", requestOptions)
-      .then(async response => {
+      .then(async (response) => {
         const data = await response.json();
         if (!response.ok) {
-          const error = (data && "Erro ao atualizar os dados do login") || response.status;
+          const error =
+            (data && "Erro ao atualizar os dados do login") || response.status;
           setLastError(data.message || error);
           setState("error");
-          pageView({page_path: '/paylment/error'});
           return Promise.reject(error);
         }
-        pageView({page_path: '/paylment/success'});
         setState("success");
       })
-      .catch(error => {
+      .catch((error) => {
         setState("error");
-        pageView({page_path: '/paylment/error'});
-        console.error('There was an error!', error);
+        console.error("There was an error!", error);
       });
   };
 
-  return <div
-    className={`${css["organism__form-user-container"]} ${className}`}
-    {...other}
-  >
-    {
-      state === "default" &&
+  return (
+    <div
+      className={`${css["organism__form-user-container"]} ${className}`}
+      {...other}
+    >
+      {state === "default" && (
         <>
-          <div className={css["container-card-form"]}>
-            <form className={css["form"]}>
-              <Input
-                required
-                label="Nome"
-                type="text"
-                autoComplete="new-password"
-                name="ProspectName"
-                className={css["column-1-3"]}
-                value={dataCard.name}
-                onChange={handleChange}
-              />
+          <form className={css["form"]}>
+            <div className={css["checked"]}>
+              {checkedList.map(({ id, name, checked }) => (
+                <>
+                  <div className={css["checked-item"]}>
+                    <label key={id}>
+                      <input
+                        type="radio"
+                        name="options"
+                        value={!checked && (dataCard.status = name)}
+                        checked={checked}
+                        onChange={(e) => changeList(id, e.target.checked)}
+                      />
+                      {name}
+                    </label>
+                  </div>
+                </>
+              ))}
+            </div>
 
-              {/* <FormRadioGroup
-                className={css["column-1-3"]}
-                label='Estado civil'
-                radioGroupName='ProspectCivilStatus'
-                radioGroupAriaLabel='civilStatus'
-                list={civilStatus}
-                selected={user.ProspectCivilStatus}
-              /> */}
+            <input
+              required
+              label="Nome"
+              autoComplete="new-password"
+              placeholder="Nome"
+              type="text"
+              name="name"
+              className={css["input"]}
+              value={dataCard.name}
+              onChange={handleChange}
+            />
 
-              <Input
-                required
-                label="Senha"
-                autoComplete="new-password"
-                type="text"
-                name="ProspectMonthlyIncome"
-                className={css["column-1-3"]}
-                value={dataCard.password}
-                onChange={handleChange}
-              />
+            <input
+              required
+              label="Senha"
+              autoComplete="new-password"
+              placeholder="Senha"
+              type="text"
+              name="password"
+              className={css["input"]}
+              value={dataCard.password}
+              onChange={handleChange}
+            />
 
-              <ButtonHorizontal
-                disabled={isFormIncomplete}
-                style="orange"
-                onClick={handleSubmit}
-                className={css['button']}
-                type="submit"
-              >
-                Login
-              </ButtonHorizontal>
-            </form>
-          </div>
+            <ButtonHorizontal
+              disabled={isFormIncomplete}
+              style="orange"
+              onClick={handleSubmit}
+              className={css["button"]}
+              type="submit"
+            >
+              Login
+            </ButtonHorizontal>
+          </form>
         </>
-  }
-  </div>;
+      )}
+    </div>
+  );
 }
 
 export default FormUser;
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import FormUser from "../../components/organisms/form-user";
-//import * as pageData from "../data/login";
+import * as pageData from "../../data/login";
 import css from "./login.module.scss";
 
 
@@ -16,7 +18,7 @@ export default function Login(props) {
   return (
     <div className={`${css["page__login-container"]} ${className}`} {...other}>
       <div className={css.column1}>
-        <FormUser/>
+        <FormUser userStatus= {pageData.UserStatus}/>
       </div>
       <div className={css.column2}>
         <img src="/images/login/children.png"></img>
